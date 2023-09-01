f45a200c08ab99e703f8b6b9f56fe8858303d2a8
KULDEEP SINGH NEGI <nkuldeep615@gmail.com>
2023-09-01 14:53:06 +0530

    OpenAiIntregrated

--- pdf_chat_app/pdf_chat/urls.py
+++ pdf_chat_app/pdf_chat/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path("" , views.home ,  name = 'home'),
     path('login/', views.login, name='login'),
     path('register/', views.register, name='register'),
-    path('logout' , views.logout_user , name='logout')
+    path('logout' , views.logout_user , name='logout'),
+    path('upload/', views.pdf_upload_view, name='upload'),
+    path('chat/', views.chat_view, name='chat'),
+    path('end_chat/', views.end_chat_view, name='end_chat')
 ]
